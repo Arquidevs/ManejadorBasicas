946222d4bb1b493e2db807ad6ad298ab34e5589c
dcamachom <you@example.com>
2023-11-16 18:59:04 -0500

    html

--- manejador_basicas/manejador_basicas/views.py
+++ manejador_basicas/manejador_basicas/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'manejador_basicas/templates/login.html')
+    return render(request, 'manejador_basicas/templates/index.html')
 
 def health_check(request):
     return JsonResponse({'message': 'OK'}, status=200)
