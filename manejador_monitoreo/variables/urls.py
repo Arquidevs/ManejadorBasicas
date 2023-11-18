69446a224d8347a0889f1d272ab5ee1fd26ddaed
DanielAcevedo99 <69821353+DanielAcevedo99@users.noreply.github.com>
2023-11-18 00:50:21 -0500

    Update urls.py

--- manejador_monitoreo/variables/urls.py
+++ manejador_monitoreo/variables/urls.py
@@ -4,6 +4,6 @@ from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 urlpatterns = [
-    path('variables/', views.variable_list, name='variableList'),
-    path('variablecreate/', csrf_exempt(views.variable_create), name='variableCreate'),
+    path('variables/', views.variable_list, name='variable_list'),
+    path('variablecreate/', csrf_exempt(views.variable_create), name='variable_create'),
 ]
