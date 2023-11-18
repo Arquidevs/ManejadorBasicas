0ce8924819d495b5267c93d341ac4e2bb353ad6f
DanielAcevedo99 <69821353+DanielAcevedo99@users.noreply.github.com>
2023-11-17 23:58:56 -0500

    Update urls.py

--- manejador_monitoreo/measurements/urls.py
+++ manejador_monitoreo/measurements/urls.py
@@ -3,6 +3,6 @@ from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 urlpatterns = [
-    path('measurement/', views.measurement_list, name='measurement'),
+    path('measurement/', views.measurement_list, name='measurement_list'),
     path('measurementcreate/', csrf_exempt(views.measurement_create), name='measurementCreate'),
 ]
