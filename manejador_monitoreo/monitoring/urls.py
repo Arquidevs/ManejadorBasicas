56fe11a72e6242afa6eb3efad246a129b505db0a
DanielAcevedo99 <69821353+DanielAcevedo99@users.noreply.github.com>
2023-11-17 22:03:28 -0500

    Update urls.py

--- manejador_monitoreo/monitoring/urls.py
+++ manejador_monitoreo/monitoring/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.index),
     path('measurements/', include('measurements.urls')),
     path('variables/', include('variables.urls')),
