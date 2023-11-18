55a76b23abe4e205895de5ee901783e7e65c4885
DanielAcevedo99 <69821353+DanielAcevedo99@users.noreply.github.com>
2023-11-18 00:08:37 -0500

    Update logic_measurement.py

--- manejador_monitoreo/measurements/logic/logic_measurement.py
+++ manejador_monitoreo/measurements/logic/logic_measurement.py
@@ -2,8 +2,8 @@ from ..models import Measurement
 from django.db.models import Avg
 
 def get_measurements():
-    queryset = Measurement.objects.all().order_by('-dateTime')[:10]
-    return (queryset)
+    queryset = Measurement.objects.all().order_by('-timestamp')[:10]
+    return queryset
 
 def create_measurement(form):
     measurement = form.save()
