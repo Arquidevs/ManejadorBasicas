5db45d5c6c51b0442154646a7ac2dcf804d56252
dcamachom <you@example.com>
2023-11-17 21:19:12 -0500

    c

--- manejador_basicas/manejador_basicas/settings.py
+++ manejador_basicas/manejador_basicas/settings.py
@@ -131,7 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = "/login/auth0"
 LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "https://facturacion.us.auth0.com/v2/logout?returnTo=http%3A%2F%2F146.148.38.228:8080"
+LOGOUT_REDIRECT_URL = "https://facturacion.us.auth0.com/v2/logout?returnTo=http%3A%2F%2F146.148.38.228:8000"
 SOCIAL_AUTH_TRAILING_SLASH = False # Remove end slash from routes
 SOCIAL_AUTH_AUTH0_DOMAIN = 'facturacion.us.auth0.com'
 SOCIAL_AUTH_AUTH0_KEY = '8wIguEUC21F5tqv66uxFqWLn2fp7Rk9S'
