b9047b4549c9ba1a95124eec416c71f282c9ee4d
DanielAcevedo99 <69821353+DanielAcevedo99@users.noreply.github.com>
2023-11-18 01:54:57 -0500

    Update urls.py

--- manejador_monitoreo/monitoring/urls.py
+++ manejador_monitoreo/monitoring/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('measurements/', include('measurements.urls')),
     path('variable-list/', include('variables.urls')),  
+    path(r'', include('django.contrib.auth.urls')),
+    path(r'', include('social_django.urls')),
 ]
 
 
