ba614dbad4c23fbab64e80d5dc6b8bf66458a70d
Gabriel Dicelis <g.dicelis@uniandes.edu.co>
2023-11-20 11:26:17 -0500

    prueba1

--- manejador_basicas/facturacion/views.py
+++ manejador_basicas/facturacion/views.py
@@ -35,7 +35,7 @@ def crear_factura(request):
                     factura.append((servicio.descripcion, precio))
                     precio_total += precio
 
-                return render(request, 'resultado_consulta.html', {
+                return render(request, 'facturacion/resultado_consulta.html', {
                     'id_factura': paciente.id,  # ID del paciente
                     'servicios_y_precios': factura,
                     'precio_total': precio_total  # Precio total
