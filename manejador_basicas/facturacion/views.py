e03adc4a080da9f0271d7f846fa58bf37213770d
Gabriel Dicelis <g.dicelis@uniandes.edu.co>
2023-12-05 21:56:58 -0500

    Update views.py

--- manejador_basicas/facturacion/views.py
+++ manejador_basicas/facturacion/views.py
@@ -69,8 +69,6 @@ def getServiciosManualTarifario (idContrato):
 def lista_pacientes(request):
     if request.method == 'GET':
         response = requests.get('http://35.193.9.218:3000/pacientes/')
-
-        return JsonResponse(response, safe=False)
         
         # Verifica si la solicitud fue exitosa (código de respuesta 200)
         if response.status_code == 200:
