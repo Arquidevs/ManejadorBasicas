a7d1458d42dbcee5c4eb1f3dbdae960436ae246f
DanielAcevedo99 <69821353+DanielAcevedo99@users.noreply.github.com>
2023-12-06 08:08:20 -0500

    Update urls.py

--- manejador_basicas/manejador_basicas/urls.py
+++ manejador_basicas/manejador_basicas/urls.py
@@ -23,7 +23,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index),
-    path('/facturacion/', include('facturacion.urls')),
+    path('facturacion/', include('facturacion.urls')),
     path('health/', views.health_check, name='health'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
