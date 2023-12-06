696779bed8bd2085322357782d21dff36b1599ed
DanielAcevedo99 <69821353+DanielAcevedo99@users.noreply.github.com>
2023-12-06 03:35:37 -0500

    Update settings.py

--- manejador_basicas/manejador_basicas/settings.py
+++ manejador_basicas/manejador_basicas/settings.py
@@ -146,7 +146,7 @@ SOCIAL_AUTH_AUTH0_SCOPE = [
     'role',
 ]
 AUTHENTICATION_BACKENDS = {
-    'monitoring.auth0backend.Auth0',
+    'facturacion.auth0backend.Auth0',
     'django.contrib.auth.backends.ModelBackend',
 }
 
