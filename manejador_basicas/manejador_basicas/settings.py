267b08a80818e8ac03a195245c0852013314b1b2
DanielAcevedo99 <69821353+DanielAcevedo99@users.noreply.github.com>
2023-12-06 04:30:34 -0500

    Update settings.py

--- manejador_basicas/manejador_basicas/settings.py
+++ manejador_basicas/manejador_basicas/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'facturacion'
+    'facturacion',
+    'social_django'
 ]
 
 MIDDLEWARE = [
