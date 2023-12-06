d332ac8d103d8a0d859d7ea7890f720916648b6a
DanielAcevedo99 <69821353+DanielAcevedo99@users.noreply.github.com>
2023-12-06 08:10:34 -0500

    Update urls.py

--- manejador_basicas/manejador_basicas/urls.py
+++ manejador_basicas/manejador_basicas/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from manejador_basicas.facturacion.views import YourSignupView
+from facturacion.views import YourSignupView
 from . import views
 from django.contrib.auth import views as auth_views
 
