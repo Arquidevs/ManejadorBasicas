c0d89efacc5de040f8ccb449af8fa3d3b89d0b45
DanielAcevedo99 <69821353+DanielAcevedo99@users.noreply.github.com>
2023-12-06 10:20:28 -0500

    .

--- manejador_monitoreo/measurements/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
-from . import views
-
-urlpatterns = [
-    path('measurement/', views.measurement_list, name='measurement_list'),
-    path('measurementcreate/', csrf_exempt(views.measurement_create), name='measurementCreate'),
-]
